cca0fa7457386112f35997572f9a65c5100eaaed
pycert <68506103+pycert@users.noreply.github.com>
2023-04-20 08:18:27 -0400

    Update streamlit_app.py
    pick

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,4 +14,6 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 st.dataframe(my_fruit_list)
