15eb82563ee28d521f9f571b11f7318d090bee1d
pycert <68506103+pycert@users.noreply.github.com>
2023-04-21 10:57:33 -0400

    Update streamlit_app.py
    requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import pandas    as pd
 import streamlit as st
+import requests
 
 st.title('My Parents New Healthy Diner')
 
@@ -19,3 +20,6 @@ fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 st.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
