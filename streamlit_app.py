4f05be124af55ea91004b5fcd545fad3e97c0073
pycert <68506103+pycert@users.noreply.github.com>
2023-04-21 15:06:55 -0400

    Update streamlit_app.py
    buttonB

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,9 +52,15 @@ if st.button('Get fruit load list'):
     my_data_rows = get_fruit_load_list()
     st.dataframe(my_data_rows)
         
-st.stop()
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into FRUIT_LOAD_LIST values('from Streamlit');")
+        return 'Thanks for adding ', new_fruit
 
 add_my_fruit = st.text_input('What fruit would you like to add?')
-st.write('Thanks for adding ', add_my_fruit)
+if st.button('Get fruit load list'):
+    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    st.text(back_from_function)
 
-my_cur.execute("insert into FRUIT_LOAD_LIST values('from Streamlit');")
+
