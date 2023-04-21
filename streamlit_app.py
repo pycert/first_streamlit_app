7e09e8efeabd7a668e82cfb5222958108523f2b9
pycert <68506103+pycert@users.noreply.github.com>
2023-04-21 11:03:29 -0400

    Update streamlit_app.py
    choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,9 @@ st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 st.dataframe(fruityvice_normalized)
