6f697909137c05d0bea556e03ec53a0898bbabe0
pycert <68506103+pycert@users.noreply.github.com>
2023-04-21 14:41:42 -0400

    Update streamlit_app.py
    error msg

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,12 +25,16 @@ st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
 
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-st.dataframe(fruityvice_normalized)
+try:
+    fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+    if not fruit_choice:
+        st.error('Please select a fruit to get information.')
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        st.dataframe(fruityvice_normalized)
+except URLError as e:
+    st.error()
 
 st.stop()
 
